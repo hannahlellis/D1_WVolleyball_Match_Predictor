b4bfb6460160ad5d5710e5acbfcde1f2bc4d63fd
hannahlellis <angusmaxwell333@gmail.com>
2025-10-27 17:57:51 -0600

    Commiting completion of web scraper. Beginning of function calls.

---
+++ compile_data.py
@@ -0,0 +1,3 @@
+from web_scrapping import scrape_ncaa_volleyball_stats
+
+data_hitting_percentage = scrape_ncaa_volleyball_stats('Hitting Percentage')
--- web_scrapping.py
+++ web_scrapping.py
@@ -1,28 +1,50 @@
-# Web Scrapping to Enable D1 Women's Volleyball Match Predictor
-# https://www.ncaa.com/robots.txt
+    # Web Scrapping to Pull D1 Women's Volleyball Stats from NCAA Website
+    # https://www.ncaa.com/robots.txt
 
-# Import necessary libraries
-import requests
-from bs4 import BeautifulSoup
+def scrape_ncaa_volleyball_stats(category_name):
+    # Import necessary libraries
+    import requests
+    from bs4 import BeautifulSoup
+    import pandas as pd
+    import time
 
-# Step 1: Fetch the webpage
-url = "https://www.ncaa.com/stats/volleyball-women/d1/current/team/45"
-response = requests.get(url)
-html_content = response.text
+    # Step 0: Which stats to pull?
+    catergories = {'Hitting Percentage' : '45', 'Kills Per Set' : '46', 'Match W-L Pctg.' : '51',}
 
-# Step 2: Parse the HTML content
-soup = BeautifulSoup(html_content, 'html.parser')
+    # Step 1: Fetch the webpage
+    url = "https://www.ncaa.com/stats/volleyball-women/d1/current/team/"
+    url_params = ["", "/p2", "/p3", "/p4", "/p5", "/p6", "/p7"]
 
-# Step 3: Find relevant data
-stats_table = soup.find('div', class_='stats-wrap')
-header_row = soup.find_all('th')
-data_rows = soup.find_all('td')
+    data_df = pd.DataFrame()
+    catergory = catergories['Hitting Percentage']
 
-# Step X: Extract relevant data
-header_values = [header.get_text(strip=True) for header in header_row]
-data_values = [data.get_text(strip=True) for data in data_rows]
-print("Headers:", header_values)
-print("Data Values:", data_values)
+    for param in url_params:
+        
+        response = requests.get(url + catergory + param)
+        html_content = response.text
 
-# Data storage
+        # Step 2: Parse the HTML content
+        soup = BeautifulSoup(html_content, 'html.parser')
 
+        # Step 3: Find relevant data
+        stats_table = soup.find('div', class_='stats-wrap')
+
+        data_rows = soup.find_all('td')
+
+        # Step X: Extract relevant data
+        data_values = [data.get_text(strip=True) for data in data_rows]
+
+        # Data storage
+        temp_data_df = pd.DataFrame([data_values[i:i+7] for i in range(0, len(data_values), 7)])
+        data_df = pd.concat([data_df, temp_data_df], ignore_index=True)
+
+        # Wait
+        time.sleep(3)  # Be respectful to the server
+
+    # Add header row
+    header_row = soup.find_all('th')
+    header_values = [header.get_text(strip=True) for header in header_row]
+    data_df.columns = header_values
+
+    # Return DataFrame
+    return data_df

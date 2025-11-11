01e9e34980780a4b553ba44cbfd204c9a90a80a2
hannahlellis <angusmaxwell333@gmail.com>
2025-11-10 18:42:05 -0700

    commit

--- compile_data.py
+++ compile_data.py
@@ -1,3 +1,26 @@
 from web_scrapping import scrape_ncaa_volleyball_stats
 
-data_hitting_percentage = scrape_ncaa_volleyball_stats('Hitting Percentage')
+# Win-Loss Percentage is output. Select input catergories to pull.
+catergory_names = ['Hitting Percentage', 'Match W-L Pctg.']
+for catergory in catergory_names:
+
+    temp_data = scrape_ncaa_volleyball_stats(catergory)
+
+    if catergory == 'Hitting Percentage':
+        data_hp = temp_data
+    elif catergory == 'Kills Per Set':
+        data_kps = temp_data
+    elif catergory == 'Assists Per Set':
+        data_aps = temp_data
+    elif catergory == 'Blocks Per Set':
+        data_bps = temp_data
+    elif catergory == 'Opposing Hitting Pctg':
+        data_ohp = temp_data
+    elif catergory == 'Match W-L Pctg.':
+        data_wlp = temp_data
+
+# Combine input(s) and output into single DataFrame for model training
+data = data_hp + data_kps + data_aps + data_bps + data_ohp + data_wlp
+
+print(data)
+
--- web_scrapping.py
+++ web_scrapping.py
@@ -1,50 +1,55 @@
     # Web Scrapping to Pull D1 Women's Volleyball Stats from NCAA Website
     # https://www.ncaa.com/robots.txt
 
-def scrape_ncaa_volleyball_stats(category_name):
-    # Import necessary libraries
-    import requests
-    from bs4 import BeautifulSoup
-    import pandas as pd
-    import time
 
-    # Step 0: Which stats to pull?
-    catergories = {'Hitting Percentage' : '45', 'Kills Per Set' : '46', 'Match W-L Pctg.' : '51',}
+# Import necessary libraries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+def scrape_ncaa_volleyball_stats(catergory_name):
+
+    catergories = {
+        'Hitting Percentage' : '45', 
+        'Kills Per Set' : '46',
+        'Assists Per Set' : '47',
+        'Blocks Per Set' : '49',
+        'Opposing Hitting Pctg' : '911', 
+        'Match W-L Pctg.' : '51',
+        }
 
     # Step 1: Fetch the webpage
-    url = "https://www.ncaa.com/stats/volleyball-women/d1/current/team/"
-    url_params = ["", "/p2", "/p3", "/p4", "/p5", "/p6", "/p7"]
+    url_main = "https://www.ncaa.com/stats/volleyball-women/d1/current/team/"
+    url_revisions = ["", "/p2", "/p3", "/p4", "/p5", "/p6", "/p7"]
 
+    # Step 2: Scrape website for data from specified catergory
+    catergory = catergories[catergory_name]
     data_df = pd.DataFrame()
-    catergory = catergories['Hitting Percentage']
 
-    for param in url_params:
+    for url in url_revisions:
         
-        response = requests.get(url + catergory + param)
+        response = requests.get(url_main + catergory + url)
         html_content = response.text
 
-        # Step 2: Parse the HTML content
+        # Parse the HTML content
         soup = BeautifulSoup(html_content, 'html.parser')
 
-        # Step 3: Find relevant data
+        # Find relevant data
         stats_table = soup.find('div', class_='stats-wrap')
-
         data_rows = soup.find_all('td')
-
-        # Step X: Extract relevant data
+        header_row = soup.find_all('th')
+    
+        # Extract relevant data
         data_values = [data.get_text(strip=True) for data in data_rows]
+        header_values = [header.get_text(strip=True) for header in header_row]
 
-        # Data storage
-        temp_data_df = pd.DataFrame([data_values[i:i+7] for i in range(0, len(data_values), 7)])
+        # Store relevant data
+        temp_data_df = pd.DataFrame([data_values[i:i+7] for i in range(0, len(data_values), len(header_values))])
         data_df = pd.concat([data_df, temp_data_df], ignore_index=True)
-
+        data_df.columns = header_values
         # Wait
-        time.sleep(3)  # Be respectful to the server
-
-    # Add header row
-    header_row = soup.find_all('th')
-    header_values = [header.get_text(strip=True) for header in header_row]
-    data_df.columns = header_values
+        time.sleep(5)  # Be respectful to the server
 
     # Return DataFrame
     return data_df
